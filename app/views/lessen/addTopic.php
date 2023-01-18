fc881e8e20065add8e186df3569faf436f2bf272
Glitchula <65823161+Glitchula@users.noreply.github.com>
2023-01-18 14:37:20 +0100

    2
    2

--- app/views/lessen/addTopic.php
+++ app/views/lessen/addTopic.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h3><?= $data['title']; ?></h3>
     <form action="<?= URLROOT; ?>/lessen/addTopic" method="post">
@@ -14,5 +16,7 @@
         <input type="hidden" name="id" value="<?= $data['id']; ?>">
         <input type="submit" value="Toevoeging">
     </form>
+    s
 </body>
+
 </html>
